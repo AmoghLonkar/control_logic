e153288202d815301bad6e8c25a3df128571b17b
Amogh Lonkar <alonkar@kaffirlime.soe.ucsc.edu>
2021-06-03 00:41:41 -0700

    Added test for multicycle read from GPIO

--- src/test/scala/control_logic/openramTestSuite.scala
+++ src/test/scala/control_logic/openramTestSuite.scala
@@ -44,7 +44,7 @@ class OpenramTestChipTester extends FreeSpec with ChiselScalatestTester {
   }
 
   "OpenramTestChip should write to SRAM 1" in {
-    test(new openram_testchip) { dut =>    
+    test(new openram_testchip).withAnnotations(Seq(WriteVcdAnnotation)) { dut =>    
         //Writing 1 to address 1 in SRAM 1  
         val packet = BigInt("080000000E020000000200", 16)
         val MOD = (Seq.fill(54)(BigInt(2)).reduce(_*_) )
@@ -59,7 +59,7 @@ class OpenramTestChipTester extends FreeSpec with ChiselScalatestTester {
   }
 
   "OpenramTestChip should read from R/W Port in SRAM 0" in {
-    test(new openram_testchip) { dut =>    
+    test(new openram_testchip).withAnnotations(Seq(WriteVcdAnnotation)) { dut =>    
         //Reading from address 1 in SRAM 0  
         val packet = BigInt("20020000000100", 16)
         val MOD = (Seq.fill(54)(BigInt(2)).reduce(_*_) )
@@ -73,12 +73,37 @@ class OpenramTestChipTester extends FreeSpec with ChiselScalatestTester {
         dut.clock.step()
         dut.io.sram0_rw_in.poke(1.U)
         dut.clock.step()
-        dut.io.sram_data.expect(1.U)
+        dut.io.la_data.expect(1.U)
+    }
+  }
+
+  "OpenramTestChip should read from R/W Port in SRAM 0 (GPIO)" in {
+    test(new openram_testchip).withAnnotations(Seq(WriteVcdAnnotation)) { dut =>    
+        //Reading from address 1 in SRAM 0  
+        val packet = BigInt("20020000000100", 16)
+        val MOD = (Seq.fill(54)(BigInt(2)).reduce(_*_) )
+        val MASK = (Seq.fill(55)(BigInt(2)).reduce(_*_) - 1)
+        dut.io.logical_analyzer_packet.poke(0.U)
+        dut.io.gpio_packet.poke(BigInt("00000100", 16).U)
+        dut.io.in_select.poke(true.B)
+        dut.clock.step()
+        dut.io.gpio_packet.poke(BigInt("00200200", 16).U)
+        dut.clock.step()
+        dut.io.gpio_packet.poke(BigInt("00000000", 16).U)
+        dut.clock.step(2)
+        dut.io.sram0_connections.expect((packet % MOD).U)
+        dut.io.sram1_connections.expect(MASK.U)
+        dut.clock.step()
+        dut.io.sram0_rw_in.poke(1.U)
+        dut.clock.step(2)
+        dut.io.gpio_data.expect(1.U)
+        dut.clock.step()
+        dut.io.gpio_data.expect(0.U)
     }
   }
 
   "OpenramTestChip should write to shared R/W Port in SRAM 2" in {
-    test(new openram_testchip) { dut =>    
+    test(new openram_testchip).withAnnotations(Seq(WriteVcdAnnotation)) { dut =>    
         //Writing 1 to address 1 in SRAM 1  
         val packet = BigInt("100000000E020000000200", 16)
         val MOD = (Seq.fill(47)(BigInt(2)).reduce(_*_) )
